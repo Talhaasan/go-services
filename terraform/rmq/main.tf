92151bb1ae456341b6a23a33e1cc4138fd10a380
Talhaasan <talha.asan@outlook.com>
2022-03-11 15:14:46 +0300

    continue to order Terraform file structure

--- terraform/rmq/main.tf
+++ terraform/rmq/main.tf
@@ -1,16 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
-  profile = var.profile
+  region     = "us-east-1"
 }
 
 resource "aws_instance" "rmq" {
-  ami = "ami-07ebfd5b3428b6f4d"
+  ami           = "ami-07ebfd5b3428b6f4d"
   instance_type = "t2.micro"
   key_name = "rabbitmq"
   vpc_security_group_ids = ["sg-0e6f56a8099ee33e4"]
-
-  tags = {
-    Name = var.name
-    group = var.group
-  }
 }

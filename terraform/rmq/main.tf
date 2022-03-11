8e0b6c3152582f7ff53dae4b514948803dfa730a
Talhaasan <talha.asan@outlook.com>
2022-03-11 17:50:22 +0300

    order main.tf

--- terraform/rmq/main.tf
+++ terraform/rmq/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "rmq" {
-  ami           = "ami-07ebfd5b3428b6f4d"
+  ami           = "ami-0e472ba40eb589f49"
   instance_type = "t2.micro"
   key_name = "rabbitmq"
   vpc_security_group_ids = ["sg-0e6f56a8099ee33e4"]
